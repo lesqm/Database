a3d475f15aec6f1e55c5f66b7004282d5f218ee6
Artem Shurygin <show.vars@gmail.com>
2015-11-30 20:40:51 +1000

    Bugfix

--- src/main/java/ru/lesqm/db/controllers/EditController.java
+++ src/main/java/ru/lesqm/db/controllers/EditController.java
@@ -5,6 +5,8 @@ import com.bunjlabs.fugaframework.foundation.Response;
 import com.bunjlabs.fugaframework.sessions.Session;
 import com.bunjlabs.fugaframework.templates.TemplateNotFoundException;
 import com.bunjlabs.fugaframework.templates.TemplateRenderException;
+import com.google.gson.Gson;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +15,21 @@ import ru.lesqm.db.logic.Database;
 import ru.lesqm.db.logic.Keyword;
 import ru.lesqm.db.logic.MClass;
 import ru.lesqm.db.logic.Molecule;
-
+import ru.lesqm.db.logic.MoleculeJson;
 
 public class EditController extends Controller {
+
+    private static final Gson gson = new Gson();
+
+    public static class JsonOk {
+
+        public JsonOk(String url) {
+            this.url = url;
+        }
+
+        public String status = "ok";
+        public String url;
+    }
 
     public Response add() throws TemplateNotFoundException, TemplateRenderException {
         return ok(view("add.html", new EditorData(forms.generateFormId("add"), null)));
@@ -23,65 +37,41 @@ public class EditController extends Controller {
 
     public Response addProcess() throws TemplateNotFoundException, TemplateRenderException {
         Session session = ctx.getSession();
-        
+
         final Database db = ((LesqmDatabaseApp) ctx.getApp()).getDatabase();
 
-        Map<String, List<String>> post = ctx.getRequest().getParameters();
-
-        if (post.get("formula") == null
-                || post.get("picture") == null
-                || post.get("baseName") == null
-                || post.get("nomeName") == null
-                || post.get("nomeNameEng") == null
-                || post.get("codeName") == null
-                || post.get("content") == null
-                || post.get("literature") == null
-                || post.get("keywords") == null
-                || post.get("classes") == null
-                || post.get("fid") == null) {
-            return ok("Bad post parameters " + post.size());
-        }
-
-        for (Map.Entry<String, List<String>> e : post.entrySet()) {
-            if (e.getValue().isEmpty()) {
-                return ok("Bad post parameter: " + e.getKey());
-            }
-        }
-
-        if (!forms.testFormId("add", post.get("fid").get(0))) {
-            return ok("Bad form id");
-        }
+        MoleculeJson inputMol = gson.fromJson(ctx.getRequest().getContent().toString(Charset.forName("UTF-8")), MoleculeJson.class);
 
         Molecule m = new Molecule();
 
-        m.setFormula(post.get("formula").get(0));
-        m.setPicture(post.get("picture").get(0));
-        m.setBaseName(post.get("baseName").get(0));
-        m.setNomeName(post.get("nomeName").get(0));
-        m.setNomeNameEng(post.get("nomeNameEng").get(0));
-        m.setCodeName(post.get("codeName").get(0));
-        m.setContent(post.get("content").get(0));
-        m.setLiterature(post.get("literature").get(0));
+        m.setFormula(inputMol.formula);
+        m.setPicture(inputMol.picture);
+        m.setBaseName(inputMol.baseName);
+        m.setNomeName(inputMol.nomeName);
+        m.setNomeNameEng(inputMol.nomeNameEng);
+        m.setCodeName(inputMol.codeName);
+        m.setContent(inputMol.content);
+        m.setLiterature(inputMol.literature);
         m.setCtype(1);
         m.setUid((long) session.get("user-id"));
 
         long id = db.putMolecule(m);
 
-        String[] keywords = post.get("keywords").get(0).split(",");
+        String[] keywords = inputMol.keywords.split(",");
         List<Long> kids = new ArrayList<>();
         for (String keyword : keywords) {
             kids.add(db.putKeyword(keyword.trim()));
         }
         db.putKeywordsToMoleculeById(id, kids);
 
-        String[] classes = post.get("classes").get(0).split(",");
+        String[] classes = inputMol.classes.split(",");
         List<Long> clids = new ArrayList<>();
         for (String mclass : classes) {
             clids.add(db.putClass(mclass.trim()));
         }
         db.putClassesToMoleculeById(id, clids);
 
-        return redirect(urls.that("change/" + Molecule.hid.encode(id)));
+        return ok(gson.toJson(new JsonOk(urls.that("change/" + Molecule.hid.encode(id))))).asJson();
     }
 
     public Response edit(String hmid) throws TemplateNotFoundException, TemplateRenderException {
@@ -102,7 +92,6 @@ public class EditController extends Controller {
     public Response editProcess(String hmid) throws TemplateNotFoundException, TemplateRenderException {
         Session session = ctx.getSession();
 
-
         final Database db = ((LesqmDatabaseApp) ctx.getApp()).getDatabase();
 
         long[] mid = Molecule.hmid.decode(hmid.toLowerCase());
@@ -112,52 +101,28 @@ public class EditController extends Controller {
             return notFoundDefault(ctx);
         }
 
-        Map<String, List<String>> post = ctx.getRequest().getParameters();
+        MoleculeJson inputMol = gson.fromJson(ctx.getRequest().getContent().toString(Charset.forName("UTF-8")), MoleculeJson.class);
 
-        if (post.get("formula") == null
-                || post.get("picture") == null
-                || post.get("baseName") == null
-                || post.get("nomeName") == null
-                || post.get("nomeNameEng") == null
-                || post.get("codeName") == null
-                || post.get("content") == null
-                || post.get("literature") == null
-                || post.get("keywords") == null
-                || post.get("classes") == null
-                || post.get("fid") == null) {
-            return ok("Bad post parameters " + post.size());
-        }
-
-        for (Map.Entry<String, List<String>> e : post.entrySet()) {
-            if (e.getValue().isEmpty()) {
-                return ok("Bad post parameter: " + e.getKey());
-            }
-        }
-
-        if (!forms.testFormId("edit", post.get("fid").get(0))) {
-            return ok("Bad form id");
-        }
-
-        m.setFormula(post.get("formula").get(0));
-        m.setPicture(post.get("picture").get(0));
-        m.setBaseName(post.get("baseName").get(0));
-        m.setNomeName(post.get("nomeName").get(0));
-        m.setNomeNameEng(post.get("nomeNameEng").get(0));
-        m.setCodeName(post.get("codeName").get(0));
-        m.setContent(post.get("content").get(0));
-        m.setLiterature(post.get("literature").get(0));
+        m.setFormula(inputMol.formula);
+        m.setPicture(inputMol.picture);
+        m.setBaseName(inputMol.baseName);
+        m.setNomeName(inputMol.nomeName);
+        m.setNomeNameEng(inputMol.nomeNameEng);
+        m.setCodeName(inputMol.codeName);
+        m.setContent(inputMol.content);
+        m.setLiterature(inputMol.literature);
         m.setCtype(1);
         m.setUid((long) session.get("user-id"));
 
         long id = db.putMoleculeChange(m);
 
-        String[] keywords = post.get("keywords").get(0).split(",");
+        String[] keywords = inputMol.keywords.split(",");
         List<Long> kids = new ArrayList<>();
         for (String keyword : keywords) {
             kids.add(db.putKeyword(keyword.trim()));
         }
 
-        String[] classes = post.get("classes").get(0).split(",");
+        String[] classes = inputMol.classes.split(",");
         List<Long> clids = new ArrayList<>();
         for (String mclass : classes) {
             clids.add(db.putClass(mclass.trim()));
@@ -166,8 +131,8 @@ public class EditController extends Controller {
         db.putKeywordsToMoleculeById(id, kids);
         db.putClassesToMoleculeById(id, clids);
 
-        return redirect(urls.that("view/" + Molecule.hmid.encode(m.getMid())
-                + "/" + Molecule.hid.encode(id)));
+        return ok(gson.toJson(new JsonOk(urls.that("view/" + Molecule.hmid.encode(m.getMid())
+                + "/" + Molecule.hid.encode(id))))).asJson();
     }
 
     public Response deleteProcess(String hmid) throws TemplateNotFoundException, TemplateRenderException {
---
+++ src/main/java/ru/lesqm/db/logic/MoleculeJson.java
@@ -0,0 +1,17 @@
+package ru.lesqm.db.logic;
+
+public class MoleculeJson {
+
+    public String formula;
+    public String picture;
+    public String baseName;
+    public String nomeName;
+    public String nomeNameEng;
+    public String codeName;
+    public String content;
+    public String literature;
+    
+    public String keywords;
+    public String classes;
+    
+}
